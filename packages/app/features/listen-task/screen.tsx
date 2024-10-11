e8696c95f28fede8af7c0d0858f8c1f3804687c1
namhndev <nguyenhoangnam31082000@gmail.com>
2024-10-12 01:30:38 +0700

    add

--- packages/app/features/listen-task/screen.tsx
+++ packages/app/features/listen-task/screen.tsx
@@ -1,31 +1,78 @@
 import React, { useState, useRef, useEffect } from 'react'
 import {
-  View,
-  Text,
   TouchableOpacity,
-  Animated,
   ScrollView,
-  TextInput,
-} from 'react-native'
+} from 'app/design/total-design'
+import { View } from 'app/design/view'
+import { Text } from 'app/design/typography'
+import { ActivityIndicator, Animated } from 'react-native'
 import { SafeAreaView } from 'app/design/total-design'
 import { useNavigation } from '@react-navigation/native'
+import Tts from 'react-native-tts'
+import { useReadTaskStore } from 'app/utils/read-task/zustand'
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 export default function ListenTask() {
   const [isSpeaking, setIsSpeaking] = useState(false)
-  const [customText, setCustomText] = useState('')
   const navigation = useNavigation()
   const animationValues = useRef(
-    [...Array(8)].map(() => new Animated.Value(0)),
+    [...Array(18)].map(() => new Animated.Value(0)),
   ).current
   const animationRef = useRef<Animated.CompositeAnimation | null>(null)
+
+  const {
+    passage,
+    question,
+    userAnswer,
+    response,
+    error,
+    isLoading,
+    setUserAnswer,
+    getPassageAndQuestion,
+    handleSubmit,
+    handleRefresh,
+  } = useReadTaskStore()
 
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: true,
-      headerTitle: 'Nghe và Phát Âm Nâng Cao',
-      headerBackTitle: 'Quay lại',
+      headerTitle: 'Listen Task',
+      headerBackTitle: 'Back',
+      headerRight: () => (
+        <Ionicons
+          name="ios-arrow-forward"
+          size={20}
+          color="#0051e3"
+          onPress={handleRefresh}
+        />
+      ),
     })
-  }, [navigation])
+  }, [navigation, handleRefresh])
+
+  useEffect(() => {
+    Tts.setDefaultLanguage('en-US')
+    const startListener = () => setIsSpeaking(true)
+    const finishListener = () => {
+      setIsSpeaking(false)
+      stopAnimation()
+    }
+    const cancelListener = () => {
+      setIsSpeaking(false)
+      stopAnimation()
+    }
+    
+    Tts.addEventListener('tts-start', startListener)
+    Tts.addEventListener('tts-finish', finishListener)
+    Tts.addEventListener('tts-cancel', cancelListener)
+    
+    getPassageAndQuestion()
+
+    // return () => {
+    //   Tts.removeEventListener('tts-start', startListener)
+    //   Tts.removeEventListener('tts-finish', finishListener)
+    //   Tts.removeEventListener('tts-cancel', cancelListener)
+    // }
+  }, [getPassageAndQuestion])
 
   const startAnimation = () => {
     const animations = animationValues.map((value, index) =>
@@ -58,30 +105,26 @@ export default function ListenTask() {
   }
 
   const speak = async () => {
-    const thingToSay =
-      customText ||
-      'Xin chào, đây là một ví dụ về chức năng đọc văn bản.'
-
+    const thingToSay = passage
+    console.log(thingToSay)
     if (isSpeaking) {
+      Tts.stop()
       setIsSpeaking(false)
       stopAnimation()
     } else {
-      setIsSpeaking(true)
       startAnimation()
       try {
-        // Thực hiện chức năng phát âm ở đây (không sử dụng expo-speech)
-        console.log('Đang phát âm:', thingToSay)
-        // Giả lập thời gian phát âm
-        setTimeout(() => {
-          setIsSpeaking(false)
-          stopAnimation()
-        }, 3000)
+        await Tts.speak(thingToSay)
       } catch (error) {
         console.error('Lỗi khi phát âm:', error)
         setIsSpeaking(false)
         stopAnimation()
       }
     }
+  }
+
+  const handleAnswerSelect = (answer: string) => {
+    setUserAnswer(answer)
   }
 
   return (
@@ -110,25 +153,91 @@ export default function ListenTask() {
               />
             ))}
           </View>
-          <TextInput
-            className="mb-4 w-full rounded-lg border border-gray-300 bg-white p-2"
-            placeholder="Nhập văn bản tùy chỉnh ở đây"
-            value={customText}
-            onChangeText={setCustomText}
-            multiline
-          />
-          <TouchableOpacity
-            onPress={speak}
-            className="mt-8 rounded-full bg-blue-500 px-8 py-4"
-          >
-            <Text className="text-lg font-bold text-white">
-              {isSpeaking ? 'Dừng' : 'Phát lại'}
-            </Text>
-          </TouchableOpacity>
-          <Text className="mt-4 text-center text-gray-600">
-            Nhấn nút để nghe văn bản được đọc bằng tiếng Việt. Bạn có thể tùy
-            chỉnh văn bản.
-          </Text>
+          {isLoading ? (
+            <ActivityIndicator size="large" color="#0051e3" />
+          ) : (
+            <>
+              <TouchableOpacity
+                onPress={speak}
+                className="mb-4 rounded-full bg-blue-500 px-8 py-4"
+              >
+                <Text className="text-lg font-bold text-white">
+                  {isSpeaking ? 'Dừng' : 'Nghe đoạn văn'}
+                </Text>
+              </TouchableOpacity>
+              {!response && (
+                <View className="w-full rounded-lg bg-white p-4 shadow-lg">
+                  <Text className="mb-2 text-lg font-bold text-blue-900">Câu hỏi:</Text>
+                  <Text className="mb-2 text-base font-semibold">
+                    {question.question}
+                  </Text>
+                  {question.options.map((option, oIndex) => (
+                    <TouchableOpacity
+                      key={oIndex}
+                      onPress={() =>
+                        handleAnswerSelect(String.fromCharCode(65 + oIndex))
+                      }
+                      className="mb-2 flex-row items-center"
+                    >
+                      <View
+                        className={`h-5 w-5 rounded-full border-2 ${
+                          userAnswer === String.fromCharCode(65 + oIndex)
+                            ? 'border-blue-500 bg-blue-500'
+                            : 'border-gray-400'
+                        } mr-2`}
+                      />
+                      <Text>
+                        {String.fromCharCode(65 + oIndex)}. {option}
+                      </Text>
+                    </TouchableOpacity>
+                  ))}
+                </View>
+              )}
+              <TouchableOpacity
+                onPress={response ? handleRefresh : handleSubmit}
+                className="mt-4 rounded bg-blue-500 px-8 py-3"
+              >
+                <Text className="text-center font-bold text-white">
+                  {response ? "Câu hỏi tiếp theo" : "Gửi câu trả lời"}
+                </Text>
+              </TouchableOpacity>
+            </>
+          )}
+          {error && (
+            <View className="mt-4 w-full rounded-lg bg-red-100 p-4 shadow-lg">
+              <Text className="text-base font-semibold text-red-800">
+                {error}
+              </Text>
+            </View>
+          )}
+          {response && (
+            <View className="mt-4 w-full rounded-lg bg-white p-4 shadow-lg">
+              <Text className="mb-2 text-lg font-bold text-blue-900">
+                Kết quả và Giải thích:
+              </Text>
+              <Text className="text-base leading-relaxed text-gray-800">
+                {response.split('##').map((part, partIndex) => (
+                  <React.Fragment key={partIndex}>
+                    {partIndex > 0 && (
+                      <Text className="mt-2 text-lg font-bold">##</Text>
+                    )}
+                    {part.split('**').map((subPart, subIndex) => (
+                      <Text
+                        key={subIndex}
+                        style={
+                          subIndex % 2 === 1
+                            ? { fontWeight: 'bold', color: 'blue' }
+                            : {}
+                        }
+                      >
+                        {subPart}
+                      </Text>
+                    ))}
+                  </React.Fragment>
+                ))}
+              </Text>
+            </View>
+          )}
         </View>
       </ScrollView>
     </SafeAreaView>
--- packages/app/features/write-task/screen.tsx
+++ packages/app/features/write-task/screen.tsx
@@ -16,24 +16,121 @@ import {
   TouchableOpacity,
 } from 'react-native'
 import { useWriteTaskStore } from 'app/utils/write-task/zustand-store'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
-export default function WriteTask() {
-  const navigation = useNavigation()
+const Tab = createBottomTabNavigator()
+
+function InputScreen() {
   const {
     input,
     setInput,
+    handleSubmit,
+  } = useWriteTaskStore()
+
+  return (
+    <View className="flex-1 bg-white">
+      <View className="flex-1 p-4">
+        <TextInput
+          className="flex-1 text-sm"
+          multiline={true}
+          placeholder="Nhập câu trả lời của bạn ở đây..."
+          value={input}
+          onChangeText={(newText) => setInput(newText)}
+          textAlignVertical="top"
+        />
+      </View>
+      <View className="items-end border-gray-200 bg-gray-100 p-2">
+        <TouchableOpacity 
+          onPress={handleSubmit}
+          className="rounded-md bg-blue-500 px-4 py-2"
+        >
+          <Text className="font-bold text-white items-center">Gửi</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+function ContentScreen() {
+  const {
     responses,
     errors,
     question,
     isLoading,
-    getQuestion,
-    handleSubmit,
-    handleRefresh
   } = useWriteTaskStore()
 
-  const navigatorBack = () => {
-    navigation.goBack()
+  if (isLoading) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size="large" color="#0051e3" />
+        <Text className="mt-4 text-sm">Loading...</Text>
+      </View>
+    )
   }
+
+  return (
+    <ScrollView className="gap-2 p-3">
+      <View className="w-full bg-white p-4">
+        <Text className="text-l mb-4 font-bold">
+          AI Evaluation and Explanation:
+        </Text>
+        <View className="space-y-4">
+          {responses.map((response, index) => (
+            <View
+              key={index}
+              className="mb-1 rounded-lg border border-gray-200 bg-white p-1"
+            >
+              <Text className="text-sm leading-relaxed text-gray-800">
+                {response.split('##').map((part, partIndex) => (
+                  <React.Fragment key={partIndex}>
+                    {partIndex > 0 && <Text className="font-bold">##</Text>}
+                    {part.split('**').map((subPart, subIndex) => (
+                      <Text
+                        key={subIndex}
+                        style={
+                          subIndex % 2 === 1 ? { fontWeight: 'bold' } : {}
+                        }
+                      >
+                        {subPart}
+                      </Text>
+                    ))}
+                  </React.Fragment>
+                ))}
+              </Text>
+              {errors[index] && (
+                <View className="mt-4 border-l-4 border-red-500 bg-red-100 p-4">
+                  <Text className="font-semibold text-red-700">Error:</Text>
+                  <Text className="mt-1 text-red-600">{errors[index]}</Text>
+                </View>
+              )}
+            </View>
+          ))}
+        </View>
+      </View>
+      {question && (
+        <Text className="mb-4 rounded-lg bg-blue-100 p-4 text-sm font-bold shadow-md">
+          {question.split('##').map((part, index) => (
+            <React.Fragment key={index}>
+              {index > 0 && <Text className="font-bold">##</Text>}
+              {part.split('**').map((subPart, subIndex) => (
+                <Text
+                  key={subIndex}
+                  style={subIndex % 2 === 1 ? { fontWeight: '400' } : {}}
+                >
+                  {subPart}
+                </Text>
+              ))}
+            </React.Fragment>
+          ))}
+        </Text>
+      )}
+    </ScrollView>
+  )
+}
+
+export default function WriteTask() {
+  const navigation = useNavigation()
+  const { getQuestion, handleRefresh } = useWriteTaskStore()
 
   React.useLayoutEffect(() => {
     navigation.setOptions({
@@ -55,97 +152,30 @@ export default function WriteTask() {
     getQuestion()
   }, [])
 
-  if (isLoading) {
-    return (
-      <View className="flex-1 items-center justify-center">
-        <ActivityIndicator size="large" color="#0051e3" />
-        <Text className="mt-4 text-sm">Đang tải...</Text>
-      </View>
-    )
-  }
-
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      className="flex-1"
-      enabled={true}
-      keyboardVerticalOffset={90}
-    >
       <SafeAreaView className="flex-1">
-        <ScrollView className="gap-2 p-3">
-          <View className="w-full bg-white p-4">
-            <Text className="text-l mb-4 font-bold">
-              Đánh giá và Giải thích của AI:
-            </Text>
-            <View className="space-y-4">
-              {responses.map((response, index) => (
-                <View
-                  key={index}
-                  className="mb-1 rounded-lg border border-gray-200 bg-white p-1"
-                >
-                  <Text className="text-sm leading-relaxed text-gray-800">
-                    {response.split('##').map((part, partIndex) => (
-                      <React.Fragment key={partIndex}>
-                        {partIndex > 0 && <Text className="font-bold">##</Text>}
-                        {part.split('**').map((subPart, subIndex) => (
-                          <Text
-                            key={subIndex}
-                            style={
-                              subIndex % 2 === 1 ? { fontWeight: 'bold' } : {}
-                            }
-                          >
-                            {subPart}
-                          </Text>
-                        ))}
-                      </React.Fragment>
-                    ))}
-                  </Text>
-                  {errors[index] && (
-                    <View className="mt-4 border-l-4 border-red-500 bg-red-100 p-4">
-                      <Text className="font-semibold text-red-700">Lỗi:</Text>
-                      <Text className="mt-1 text-red-600">{errors[index]}</Text>
-                    </View>
-                  )}
-                </View>
-              ))}
-            </View>
-          </View>
-          {question && (
-            <Text className="mb-4 rounded-lg bg-blue-100 p-4 text-sm font-bold shadow-md">
-              {question.split('##').map((part, index) => (
-                <React.Fragment key={index}>
-                  {index > 0 && <Text className="font-bold">##</Text>}
-                  {part.split('**').map((subPart, subIndex) => (
-                    <Text
-                      key={subIndex}
-                      style={subIndex % 2 === 1 ? { fontWeight: '400' } : {}}
-                    >
-                      {subPart}
-                    </Text>
-                  ))}
-                </React.Fragment>
-              ))}
-            </Text>
-          )}
-        </ScrollView>
-        <View className="mb-[4px] flex-row items-center gap-4 p-2 align-bottom">
-          <TextInput
-            className="m-0 max-h-32 w-5/6 rounded-lg bg-[#d4d4d4] p-2"
-            onSubmitEditing={Keyboard.dismiss}
-            multiline={true}
-            placeholder="Enter your task"
-            editable
-            value={input}
-            onChangeText={(newText) => setInput(newText)}
-            underlineColorAndroid="transparent"
-            numberOfLines={5}
-            scrollEnabled={true}
+        <Tab.Navigator>
+          <Tab.Screen 
+            name="Content" 
+            component={ContentScreen}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="ios-document-text" color={color} size={size} />
+              ),
+            }}
           />
-          <TouchableOpacity onPress={handleSubmit}>
-            <Ionicons name="ios-send" size={20} color="#0051e3" />
-          </TouchableOpacity>
-        </View>
+          <Tab.Screen 
+            name="Input" 
+            component={InputScreen}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="ios-create" color={color} size={size} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
       </SafeAreaView>
-    </KeyboardAvoidingView>
   )
 }
